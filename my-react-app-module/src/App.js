9bcdd5b87f4f7aa6dc9c31bfd56687aed84bc9d8
Battulga Ishjamts <tulgaa.bi@gmail.com>
2023-01-08 23:51:23 +0800

    updated e-commerce but didnt finished heh

--- my-react-app-module/src/App.js
+++ my-react-app-module/src/App.js
@@ -1,21 +1,90 @@
 import './App.css';
 import MainMenu from '../src/components/MainMenu';
+import AliceCarousel from 'react-alice-carousel'
 import Upper from './components/Upper'
 import Search from './components/search'
-import productSlider from './components/productSlider';
+import ProductSlider from './components/productSlider';
+import productsData from './data/products';
+import Products from './components/Products';
+
+
+import categoryData from './data/category';
+import Category from './components/Category';
 
 function App() {
+
+
+  const categories = categoryData.map(data => {
+    return (
+      <Category
+        title={data.title}
+        imgUrl={data.imgUrl}
+        items={data.items}
+      />
+    )
+  })
+
+
+  const products = productsData.map(data => {
+    return (
+      <Products
+        title={data.title}
+        imgUrl={data.imgUrl}
+        price={data.price}
+        rating={data.rating}
+      />
+    )
+  })
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='Upper'>
-          
-          <Upper/>
-          <Search/>
-          <MainMenu/>
-          <productSlider/>
+
+          <Upper />
+          <Search />
+          <MainMenu />
+          <div className='content-container'>
+            <ProductSlider />
+
+
+            <AliceCarousel
+              responsive={{
+                0: { items: 3 }
+              }}>
+              {categories}
+            </AliceCarousel>
+
+            <div className='popular-products'>
+              <div className='popular-text'>
+                <p>Popular product</p>
+              </div>
+              <div className='popular-btn'>
+                <button>Laptops</button>
+                <button>Tablets</button>
+                <button>Mouse</button>
+              </div>
+            </div>
+
+
+
+            <AliceCarousel autoPlay autoPlayInterval="3000"
+              responsive={{
+                0: {
+                  items: 4
+                },
+              }}>
+
+              {products}
+
+            </AliceCarousel>
+
+          </div>
         </div>
-     
+
       </header>
     </div>
   );
